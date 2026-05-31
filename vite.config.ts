685f80f3cf878293ea95c2508fe3d85d4fffcf78
Daichi-xx007 <elemant13@gmail.com>
2026-05-31 16:13:41 +0500

    Fix Vercel NPM package name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@react-router/vercel/vite";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default defineConfig({
   plugins: [reactRouter({ presets: [vercelPreset()] }), tsconfigPaths()],
